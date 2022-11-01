3632f022b84195485eed403718adab61999a6a70
Leonardo Gonzalez <leogb37@gmail.com>
2022-11-01 20:16:20 -0300

    Se actualiza listar propiedades

--- Base_de_Datos/CONTROLADORES/ListarPropiedades.py
+++ Base_de_Datos/CONTROLADORES/ListarPropiedades.py
@@ -2,7 +2,7 @@
 from Conexion import conexion
 
 cursor=conexion.cursor()
-cursor.execute("SELECT * from Propiedad")
+cursor.execute("SELECT Nombre_Estado, Nombre_Tipo, P.Nombre, P.Direccion, P.Contacto FROM  Propiedad Pr inner join Estado E on  Pr.Id_Estado=E.Id_Estado inner join Propietario P on Pr.Id_Propietario=P.Id_Propietario inner join Tipo T on Pr.Id_Tipo=T.Id_Tipo;")
 for x in cursor:
     print(x)
 conexion.close()
