7dfd4be59e3a4322a476667d5e61ff4a519dc5d6
Leonardo Gonzalez <leogb37@gmail.com>
2022-11-02 00:00:18 -0300

    Se corrigen errores

--- Base_de_Datos/CONTROLADORES/ListarPropiedades.py
+++ Base_de_Datos/CONTROLADORES/ListarPropiedades.py
@@ -1,7 +1,7 @@
 from Conexion import conexion
 
 
-#def listarPropiedades(self):
+def listarPropiedades(self):
     cursor=conexion.cursor()
     cursor.execute("SELECT Nombre_Estado, Nombre_Tipo, P.Nombre, P.Direccion, P.Contacto, Pr.Nombre, Pr.Direccion, Pr.Contacto FROM  Propiedad Pr inner join Estado E on  Pr.Id_Estado=E.Id_Estado inner join Propietario P on Pr.Id_Propietario=P.Id_Propietario inner join Tipo T on Pr.Id_Tipo=T.Id_Tipo;")
     for x in cursor:
--- Base_de_Datos/CONTROLADORES/disponibleAlquiler.py
+++ Base_de_Datos/CONTROLADORES/disponibleAlquiler.py
@@ -1,9 +1,8 @@
-
 from Conexion import conexion
-
-cursor=conexion.cursor()
-cursor.execute("SELECT Nombre_Estado, Nombre_Tipo, P.Nombre, P.Direccion, P.Contacto, OPC.Nombre_Operatoria_Comercial FROM  Propiedad Pr inner join Estado E on  Pr.Id_Estado=E.Id_Estado inner join Propietario P on Pr.Id_Propietario=P.Id_Propietario inner join Tipo T on Pr.Id_Tipo=T.Id_Tipo INNER JOIN OperatoriaComercial OPC on Pr.Id_Operacion_Comercial=OPC.Id_Operatoria_Comercial Where Nombre_Operatoria_Comercial = 'Alquiler' and Nombre_Estado ='Disponible'")
-for x in cursor:
-    print(x)
-conexion.close()
-print("CONEXION CERRADA EXITOSAMENTE")
+def disponibleAlquiler(self):
+    cursor=conexion.cursor()
+    cursor.execute("SELECT Nombre_Estado, Nombre_Tipo, P.Nombre, P.Direccion, P.Contacto, OPC.Nombre_Operatoria_Comercial FROM  Propiedad Pr inner join Estado E on  Pr.Id_Estado=E.Id_Estado inner join Propietario P on Pr.Id_Propietario=P.Id_Propietario inner join Tipo T on Pr.Id_Tipo=T.Id_Tipo INNER JOIN OperatoriaComercial OPC on Pr.Id_Operacion_Comercial=OPC.Id_Operatoria_Comercial Where Nombre_Operatoria_Comercial = 'Alquiler' and Nombre_Estado ='Disponible'")
+    for x in cursor:
+        print(x)
+    conexion.close()
+    print("CONEXION CERRADA EXITOSAMENTE")
