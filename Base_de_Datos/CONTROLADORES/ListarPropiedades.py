e9b92aff75469b280107ef7f9fd765b7a78f67a6
Leonardo Gonzalez <leogb37@gmail.com>
2022-11-01 23:05:10 -0300

    Se agregan archivos de consulta de propiedades

--- Base_de_Datos/CONTROLADORES/ListarPropiedades.py
+++ Base_de_Datos/CONTROLADORES/ListarPropiedades.py
@@ -1,9 +1,12 @@
-
 from Conexion import conexion
 
-cursor=conexion.cursor()
-cursor.execute("SELECT Nombre_Estado, Nombre_Tipo, P.Nombre, P.Direccion, P.Contacto FROM  Propiedad Pr inner join Estado E on  Pr.Id_Estado=E.Id_Estado inner join Propietario P on Pr.Id_Propietario=P.Id_Propietario inner join Tipo T on Pr.Id_Tipo=T.Id_Tipo;")
-for x in cursor:
-    print(x)
-conexion.close()
-print("CONEXION CERRADA EXITOSAMENTE")
+
+#def listarPropiedades(self):
+    cursor=conexion.cursor()
+    cursor.execute("SELECT Nombre_Estado, Nombre_Tipo, P.Nombre, P.Direccion, P.Contacto, Pr.Nombre, Pr.Direccion, Pr.Contacto FROM  Propiedad Pr inner join Estado E on  Pr.Id_Estado=E.Id_Estado inner join Propietario P on Pr.Id_Propietario=P.Id_Propietario inner join Tipo T on Pr.Id_Tipo=T.Id_Tipo;")
+    for x in cursor:
+        print(x)
+    conexion.close()
+    print("CONEXION CERRADA EXITOSAMENTE")
+
+
